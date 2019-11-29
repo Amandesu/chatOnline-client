ce60512687489a17193e2ce1ef583375da092e06
leiwuyi <leiwuyi51@qq.com>
2019-11-29 21:23:58 +0800

    修改新朋友跳转路由

--- src/routers/home/routes/addressList/index.js
+++ src/routers/home/routes/addressList/index.js
@@ -36,7 +36,7 @@ export default class AddressList extends React.Component {
                     {this.createItem(
                         <div style={{background:"orange"}}><Icon color="#fff" code="&#xe747;" size={24}/></div>,
                         "新的朋友",
-                        () => this.props.history.push("/newFriend")
+                        () => this.props.history.push("/friend/add")
                     )}
                     {this.createItem(
                         <div style={{background:"green"}}><Icon color="#fff" code="&#xe747;" size={24}/></div>,
